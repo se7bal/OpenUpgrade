d35a7d466bce3638ec38465e125d8c1d309f3876
gmartorell <48567939+gmartorell@users.noreply.github.com>
2025-10-13 09:50:45 +0200

    Update post-migration.py

--- openupgrade_scripts/scripts/se7_boat_area/16.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/se7_boat_area/16.0.1.0/post-migration.py
@@ -13,5 +13,7 @@ def migrate(env, version):
         """
         update account_move_line
         set boat_area_id = (select yacht_area from account_invoice_line where id = old_invoice_line_id);
+
+        insert into boat_boat_area_rel SELECT yacht_area_yacht_yacht_rel.yacht_area_id, yacht_area_yacht_yacht_rel.yacht_yacht_id FROM public.yacht_area_yacht_yacht_rel;
         """
     )
